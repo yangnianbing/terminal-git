27018a30c69f56e16922163b91ea1282bd31b7e2
yangnianbing <yangnianbing@gmail.com>
2023-03-21 07:35:07 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use anyhow::{bail, Result};
 use std::{
     io::{self, Write}
 };
-use tui::{
-    backend::{Backend, CrosstermBackend},
-    Terminal,
-};
+// use tui::{
+//     backend::{Backend, CrosstermBackend},
+//     Terminal,
+// };
 
 mod app;
 mod cmdbar;
@@ -25,10 +25,10 @@ fn run_app() {
 
 fn start_termina<W: Write>(
     buf: W
-) -> io::Result<Terminal<CrosstermBackend<W>>> {
-    let backend = CrosstermBackend::new(buf);
-    let mut termial = Terminal::new(backend)?;
-    termial.hide_cursor()?;
-    termial.clear()?;
-    Ok(termial)
+)  {
+    // let backend = CrosstermBackend::new(buf);
+    // let mut termial = Terminal::new(backend)?;
+    // termial.hide_cursor()?;
+    // termial.clear()?;
+    // Ok(termial)
 }
